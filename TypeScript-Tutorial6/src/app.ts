1bf9a7263fcde18469fa85655438c93eddb66a2d
ppletes <ppletes@gmail.com>
2021-07-07 15:15:54 +0200

    TypeScript-Tutorial6

---
+++ TypeScript-Tutorial6/src/app.ts
@@ -0,0 +1,11 @@
+// /// <reference path="component/project-input.ts" />
+// /// <reference path="component/project-list.ts" />
+import _ from "lodash";
+
+// namespace App {
+  // new ProjectInput();
+  // new ProjectList("active");
+  // new ProjectList("finished");
+
+  console.log(_.shuffle([1, 2, 3]));
+// }
